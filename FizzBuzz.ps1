68593fa7f597bd6314ba5c376fdbb2526e63655c
Tyler Applebaum <tylerapplebaum@gmail.com>
2015-07-08 07:51:20 -0700

    Version 2

--- FizzBuzz.ps1
+++ FizzBuzz.ps1
@@ -1,20 +1,20 @@
 # "Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”."
 
 # FizzBuzz challenge - Tyler Applebaum 7/7/2015
-# I took the "lazy" way out and just checked for a decimal. If there's a decimal, it must not be divisible evenly.
+# Version 2, now with more modulus.
 
 $arr = 1..100 # Define the array
 foreach ($i in $arr){ #Iterate through the array
 $FizzBuzz = 15 #If a number is divisible by both 3 and 5, then it's divisible by 15.
 $Buzz = 5
 $Fizz = 3
-  If ($($i / $FizzBuzz -notmatch '\.')){ #Define the "most restrictive" condition first
+  If ($i % 15){ #Define the "most restrictive" condition first
     Write-Output "FizzBuzz"
   }
-  Elseif ($($i / $Buzz -notmatch '\.')){ #Define the second condition
+  Elseif ($i % 5){ #Define the second condition
     Write-Output "Buzz"
   }
-  Elseif ($($i / $Fizz -notmatch '\.')){ #Define the third condition
+  Elseif ($i % 3){ #Define the third condition
     Write-Output "Fizz"
   }
   Else {
